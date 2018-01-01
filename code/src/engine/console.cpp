d880258243e0c42e66dd4d436a186844def3bfd4
Simon Hössjer <simon_hossjer@hotmail.com>
2018-01-01 13:51:45 +0100

    Split console into parts :scissors:
    Add windows and linux specifics files to reduce the amount of
    preprocessor directives.

--- code/src/engine/console.cpp
+++ code/src/engine/console.cpp
@@ -1,54 +1,28 @@
 
 #include "console.hpp"
 
-#include <core/async/delay.hpp>
-#include <core/async/Thread.hpp>
-
-#include "engine/application/window.hpp"
 #include <engine/Asset.hpp>
 #include <engine/debug.hpp>
 
 #include <algorithm>
-#include <iostream>
 #include <sstream>
 #include <unordered_map>
 #include <vector>
 
 #include <cctype>
-#include <stdio.h>
-
-#ifndef _WIN32
-# include <poll.h>
-# include <unistd.h>
-#endif
+#include <cstdio>
 
 namespace
 {
 	using namespace engine::console;
 
 	std::vector < std::pair<engine::Asset, std::unique_ptr<CallbackBase>> > observers;
-
-	core::async::Thread thread;
-
-#ifdef _WIN32
-	volatile bool active;
-	HANDLE handle = nullptr;
-#else
-	int interrupt_pipes[2];
-#endif
 }
 
 namespace engine
 {
 	namespace console
 	{
-		void observe_impl(const std::string & keyword, std::unique_ptr<CallbackBase> && callback)
-		{
-			const auto key = engine::Asset{ keyword };
-
-			observers.emplace_back(key, std::move(callback));
-		}
-
 		bool is_number_candidate(const std::string & str)
 		{
 			return str.find_first_not_of("-.0123456789") == std::string::npos;
@@ -118,102 +92,32 @@ namespace engine
 			return params;
 		}
 
-		void read_input()
+		void observe_impl(const std::string & keyword, std::unique_ptr<CallbackBase> && callback)
 		{
-#ifdef _WIN32
-			while (active)
-			{
-				std::string line;
-				if (!std::getline(std::cin, line))
-				{
-					application::window::close();
-					break;
-				}
+			const auto key = engine::Asset{ keyword };
 
-				if (line.empty())
-					continue;
-
-				std::stringstream stream{ line };
-				std::string data;
-
-				std::getline(stream, data, ' ');
-				const engine::Asset key{ data };
-
-				std::getline(stream, data);
-				std::vector<Param> params = parse_params(data);
-
-				for (auto & observer : observers)
-				{
-					if (observer.first == key)
-						observer.second->call(params);
-				}
-			}
-#else
-			struct pollfd fds[2] = {
-				{interrupt_pipes[0], 0, 0},
-				{STDIN_FILENO, POLLIN, 0}
-			};
-
-			while (true)
-			{
-				const auto n = poll(fds, 2, -1);
-
-				if (fds[0].revents & POLLHUP)
-					break;
-
-				if (fds[1].revents & POLLIN)
-				{
-					std::string line;
-					if (!std::getline(std::cin, line))
-					{
-						application::window::close();
-						break;
-					}
-
-					if (line.empty())
-						continue;
-
-					std::stringstream stream{ line };
-					std::string data;
-
-					std::getline(stream, data, ' ');
-					const engine::Asset key{ data };
-
-					std::getline(stream, data);
-					std::vector<Param> params = parse_params(data);
-
-					for (auto & observer : observers)
-					{
-						if (observer.first == key)
-							observer.second->call(params);
-					}
-				}
-			}
-			close(interrupt_pipes[0]);
-#endif
-			debug_printline("console thread stopping");
+			observers.emplace_back(key, std::move(callback));
 		}
 
-		void create()
+		void read_input(std::string line)
 		{
-#ifdef _WIN32
-			active = true;
-			handle = GetStdHandle(STD_INPUT_HANDLE);
-#else
-			pipe(interrupt_pipes);
-#endif
-			thread = core::async::Thread{ read_input };
-		}
+			if (line.empty())
+				return;
 
-		void destroy()
-		{
-#ifdef _WIN32
-			active = false;
-			CancelIoEx(handle, NULL);
-#else
-			close(interrupt_pipes[1]);
-#endif
-			thread.join();
+			std::stringstream stream{ line };
+			std::string data;
+
+			std::getline(stream, data, ' ');
+			const engine::Asset key{ data };
+
+			std::getline(stream, data);
+			std::vector<Param> params = parse_params(data);
+
+			for (auto & observer : observers)
+			{
+				if (observer.first == key)
+					observer.second->call(params);
+			}
 		}
 	}
 }
