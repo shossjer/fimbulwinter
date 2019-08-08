c4f73db9073f0e895b4ece112ae0000ce79abbb2
Simon Hössjer <simon_hossjer@hotmail.com>
2019-08-08 23:07:30 +0200

    Guard against duplicate inputs :guardsman:
    We already guard against this in `ui.cpp` but that logic is entangled
    in the parsing of the input. It would be better guard elsewhere.

--- code/src/engine/hid/input.cpp
+++ code/src/engine/hid/input.cpp
@@ -1,6 +1,8 @@
 
 #include "input.hpp"
 
+#include <bitset>
+
 namespace engine
 {
 	namespace hid
@@ -12,12 +14,35 @@ namespace engine
 	}
 }
 
+namespace
+{
+	std::bitset<engine::hid::Input::button_count> button_states;
+}
+
 namespace engine
 {
 	namespace hid
 	{
 		void dispatch(const Input & input)
 		{
+			switch (input.getState())
+			{
+			case Input::State::BUTTON_DOWN:
+				if (button_states[static_cast<int>(input.getButton())])
+					return;
+
+				button_states.set(static_cast<int>(input.getButton()));
+				break;
+			case Input::State::BUTTON_UP:
+				if (!button_states[static_cast<int>(input.getButton())])
+					return;
+
+				button_states.reset(static_cast<int>(input.getButton()));
+				break;
+			default:
+				break;
+			}
+
 			ui::notify_input(input);
 		}
 	}
