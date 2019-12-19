523930abbf9b7ecd219963a45e31523de51ca04c
Simon Hössjer <simon_hossjer@hotmail.com>
2019-12-19 17:56:30 +0100

    Add assert hook to tests :chart_with_upwards_trend:

---
+++ code/tst/main_core.cpp
@@ -0,0 +1,37 @@
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "core/debug.hpp"
+
+// todo : extend catch's console reporter to print a better message when
+// asserts fail
+//
+// https://github.com/catchorg/Catch2/blob/master/include/reporters/catch_reporter_console.cpp#L691
+
+namespace
+{
+	bool test_fail_hook()
+	{
+		FAIL("assert failed, see error above");
+		return false;
+	}
+
+	struct setup_fail_hook
+	{
+		setup_fail_hook()
+		{
+			core::debug::instance().set_fail_hook(test_fail_hook);
+		}
+	} setup_fail_hook_hack;
+}
+
+TEST_CASE( "Important test", "Make sure logic works" )
+{
+    REQUIRE((1 && 2));
+	REQUIRE_FALSE((1 & 2));
+
+	REQUIRE((1 || 2));
+	REQUIRE((1 | 2));
+	REQUIRE((1 | 2) == (1 | 3));
+}
