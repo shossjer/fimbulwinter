6697dd3d9fa44c4d3fbbaca49a723aa23278c462
Simon Hössjer <simon_hossjer@hotmail.com>
2019-12-24 02:27:19 +0100

    Add construction test of engine audio :sound:

--- code/tst/engine/audio/system.cpp
+++ code/tst/engine/audio/system.cpp
@@ -6,7 +6,10 @@
 
 TEST_CASE("Audio System can be created and destroyed", "[engine][audio]")
 {
-	engine::audio::System system;
+	for (int i = 0; i < 2; i++)
+	{
+		engine::audio::System system;
+	}
 }
 
 TEST_CASE("Audio System can play sound", "[.engine][.audio]")
