2b07c3da96e2611a17dd21e55dbe3406c04c5cb0
Simon Hössjer <simon_hossjer@hotmail.com>
2019-12-24 02:40:50 +0100

    Add construction test of engine animation :runner:

---
+++ code/tst/engine/animation/mixer.cpp
@@ -0,0 +1,34 @@
+#include <catch.hpp>
+
+#include "config.h"
+
+#include "engine/animation/mixer.hpp"
+#include "engine/application/config.hpp"
+#include "engine/application/window.hpp"
+#include "engine/graphics/renderer.hpp"
+#include "engine/graphics/viewer.hpp"
+#include "engine/physics/physics.hpp"
+#include "engine/resource/reader.hpp"
+
+#if WINDOW_USE_USER32
+# include <Windows.h>
+#endif
+
+TEST_CASE("Animation Mixer can be created and destroyed", "[engine][animation]")
+{
+	engine::resource::reader reader;
+	engine::application::config_t config;
+#if WINDOW_USE_USER32
+	engine::application::window window(GetModuleHandle(nullptr), SW_HIDE, config);
+#else
+	engine::application::window window(config);
+#endif
+	engine::graphics::renderer renderer(window, reader, nullptr, engine::graphics::renderer::Type::OPENGL_1_2);
+	engine::graphics::viewer viewer(renderer);
+	engine::physics::simulation simulation(renderer, viewer);
+
+	for (int i = 0; i < 2; i++)
+	{
+		engine::animation::mixer mixer(renderer, simulation);
+	}
+}
