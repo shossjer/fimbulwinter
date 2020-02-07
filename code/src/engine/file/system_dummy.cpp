6c8c683e18edc4e67c7b9d855e64ca569d79fe4b
Simon Hössjer <simon_hossjer@hotmail.com>
2020-02-07 22:31:20 +0100

    Fix unused variable warnings :warning:

--- code/src/engine/file/system_dummy.cpp
+++ code/src/engine/file/system_dummy.cpp
@@ -18,44 +18,44 @@ namespace engine
 		system::system()
 		{}
 
-		void register_directory(engine::Asset name, utility::heap_string_utf8 && path)
+		void register_directory(engine::Asset debug_expression(name), utility::heap_string_utf8 && debug_expression(path))
 		{
 			debug_printline("todo register_directory(", name, ", ", path, ")");
 		}
 
-		void register_temporary_directory(engine::Asset name)
+		void register_temporary_directory(engine::Asset debug_expression(name))
 		{
 			debug_printline("todo register_temporary_directory(", name, ")");
 		}
 
-		void unregister_directory(engine::Asset name)
+		void unregister_directory(engine::Asset debug_expression(name))
 		{
 			debug_printline("todo unregister_directory(", name, ")");
 		}
 
 		void read(
-			engine::Asset directory,
-			utility::heap_string_utf8 && pattern,
-			watch_callback * callback,
-			utility::any && data)
+			engine::Asset debug_expression(directory),
+			utility::heap_string_utf8 && debug_expression(pattern),
+			watch_callback * debug_expression(callback),
+			utility::any && debug_expression(data))
 		{
 			debug_printline("todo read(", directory, ", ", pattern, ", ", callback, ", ", data, ")");
 		}
 
 		void watch(
-			engine::Asset directory,
-			utility::heap_string_utf8 && pattern,
-			watch_callback * callback,
-			utility::any && data)
+			engine::Asset debug_expression(directory),
+			utility::heap_string_utf8 && debug_expression(pattern),
+			watch_callback * debug_expression(callback),
+			utility::any && debug_expression(data))
 		{
 			debug_printline("todo watch(", directory, ", ", pattern, ", ", callback, ", ", data, ")");
 		}
 
 		void write(
-			engine::Asset directory,
-			utility::heap_string_utf8 && filename,
-			write_callback * callback,
-			utility::any && data)
+			engine::Asset debug_expression(directory),
+			utility::heap_string_utf8 && debug_expression(filename),
+			write_callback * debug_expression(callback),
+			utility::any && debug_expression(data))
 		{
 			debug_printline("todo write(", directory, ", ", filename, ", ", callback, ", ", data, ")");
 		}
