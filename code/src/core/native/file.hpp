6eb7eca781d67e536712d0eed0af6e93d51c028b
Simon Hössjer <simon_hossjer@hotmail.com>
2020-02-26 23:17:37 +0100

    Add native file reading support :nerd_face:
    This is a low level interface to interact with files, and particularly
    for this commit about file reading. Most file usage should probably go
    through the file module in the engine library instead of using this
    interface directly.
    
    One notable exception where this interface actually could be the
    better choice is at start up. For instance, the main application might
    want to read settings from files before starting any engine modules,
    and since the file module is an engine module we have something like
    the bootstrapping problem. This then would be an easy workaround.

---
+++ code/src/core/native/file.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+// todo forward declare
+#include "utility/unicode.hpp"
+
+namespace core
+{
+	class ReadStream;
+}
+
+namespace core
+{
+	namespace native
+	{
+		bool try_read_file(utility::heap_string_utf8 && filepath, void (* callback)(core::ReadStream && stream, void * data), void * data);
+	}
+}
