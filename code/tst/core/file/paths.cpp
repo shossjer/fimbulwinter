cf0ab509ebcb8c26e7cef151b889c90541e9bb13
Simon Hössjer <simon_hossjer@hotmail.com>
2020-03-13 01:38:55 +0100

    Add function to extract file names :speaking_head:

---
+++ code/tst/core/file/paths.cpp
@@ -0,0 +1,14 @@
+#include "core/file/paths.hpp"
+
+#include "utility/unicode.hpp"
+
+#include <catch/catch.hpp>
+
+TEST_CASE("", "[engine][file]")
+{
+	CHECK(core::file::filename(utility::string_view_utf8(u8"path/to/some.thing")) == u8"some");
+	CHECK(core::file::filename(utility::string_view_utf8(u8"path/to/extensionless")) == u8"extensionless");
+	CHECK(core::file::filename(utility::string_view_utf8(u8"filename.extension")) == u8"filename");
+	CHECK(core::file::filename(utility::string_view_utf8(u8"without-path-and-extension")) == u8"without-path-and-extension");
+	CHECK(core::file::filename(utility::string_view_utf8(u8"")) == u8"");
+}
