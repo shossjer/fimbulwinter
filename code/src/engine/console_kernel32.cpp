fa4e3cd4f9be90c7ce4a70149b12402b2be734b5
Simon Hössjer <simon_hossjer@hotmail.com>
2022-02-06 17:51:04 +0100

    Read standard in natively :inbox_tray:

--- code/src/engine/console_kernel32.cpp
+++ code/src/engine/console_kernel32.cpp
@@ -7,8 +7,7 @@
 
 #include "engine/console.hpp"
 
-#include <iostream>
-#include <string>
+#include "ful/convert.hpp"
 
 #include <windows.h>
 
@@ -32,21 +31,66 @@ namespace
 
 namespace
 {
-	void read_input()
+	core::async::thread_return thread_decl read_input(core::async::thread_param arg)
 	{
+		static_cast<void>(arg);
+
+#if 0
+		// todo we would like to use this code but the windows console
+		// cannot read utf8
+		//
+		// https://github.com/microsoft/terminal/issues/4551
+
+		if (!debug_verify(::SetConsoleCP(65001) != FALSE, "failed with last error ", ::GetLastError())) // utf8
+			return;
+
 		while (active)
 		{
-			std::string line;
-			if (!std::getline(std::cin, line))
+			ful::unit_utf8 buffer[512]; // arbitrary
+			DWORD chars_read = 0; // note must be initialized since canceling the read will return success without modifying it
+			if (::ReadConsoleA(handle, buffer, sizeof buffer, &chars_read, nullptr) == FALSE)
 			{
+				debug_printline("ReadConsoleA failed with last error ", ::GetLastError());
+
 				callback_exit();
+
 				break;
 			}
+			if (chars_read == 0)
+				break;
+			if (!debug_assert(chars_read >= 2))
+				break;
 
-			engine::detail::read_input(ful::view_utf8(line.data(), line.size()));
+			engine::detail::read_input(ful::view_utf8(buffer + 0, chars_read - 2)); // two less to account for newline
 		}
+#else
+		while (active)
+		{
+			ful::unit_utf8 buffer[512]; // arbitrary
+			DWORD chars_read = 0; // note must be initialized since canceling the read will return success without modifying it
+			ful::unit_utfw * const begw = reinterpret_cast<ful::unit_utfw *>(buffer + (sizeof buffer - (sizeof buffer / 3) * 2));
+			if (::ReadConsoleW(handle, begw, sizeof buffer / 3, &chars_read, nullptr) == FALSE)
+			{
+				debug_printline("ReadConsoleW failed with last error ", ::GetLastError());
+
+				callback_exit();
+
+				break;
+			}
+			if (chars_read == 0)
+				break;
+			if (!debug_assert(chars_read >= 2))
+				break;
+
+			const ful::unit_utf8 * const end8 = ful::convert(begw, begw + chars_read - 2, buffer + 0); // two less to account for newline
+
+			engine::detail::read_input(ful::view_utf8(buffer + 0, end8));
+		}
+#endif
 
 		debug_printline("console thread stopping");
+
+		return core::async::thread_return{};
 	}
 }
 
@@ -55,21 +99,25 @@ namespace engine
 	console::~console()
 	{
 		active = false;
-		CancelIoEx(handle, NULL);
+		CancelIoEx(handle, nullptr);
 
 		thread.join();
+
+		debug_verify(::CloseHandle(handle) != FALSE, "failed with last error ", ::GetLastError());
 
 		::callback_exit = nullptr;
 	}
 
 	console::console(void (* callback_exit_)())
 	{
+		handle = ::CreateFileW(L"CONIN$", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_WRITE, nullptr, OPEN_EXISTING, 0, nullptr);
+		if (!debug_verify(handle != INVALID_HANDLE_VALUE, "failed with last error ", ::GetLastError()))
+			return;
+
 		::callback_exit = callback_exit_;
 
 		active = true;
-		handle = GetStdHandle(STD_INPUT_HANDLE);
-
-		thread = core::async::Thread(::read_input);
+		thread = core::async::Thread(::read_input, nullptr);
 	}
 }
 
